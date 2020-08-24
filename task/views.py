001f39ef2350d11366a548cbf54cef3ae84c630a
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-08-24 23:20:38 +0530

    intial commit

---
+++ task/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Task
+
+# Create your views here.
+def home(request):
+    
+
+    tasks = Task.objects.all()
+    return render(request,'index.html',{'tasks': tasks})
